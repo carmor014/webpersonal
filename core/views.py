54237eaf8afafcb643469dec0c07571bd8c8c0bf
Carmor014 <carmor_014@hotmail.com>
2020-05-26 07:57:04 -0500

    Herencia en plantillas

---
+++ core/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, HttpResponse
+
+htmal_base = """
+<h1>Chorizos Charala</h1>
+<ul>
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about-me/">Acerca de</a></li>
+    <li><a href="/portafolio/">Portafolio</a></li>
+    <li><a href="/contacto/">Contactanos</a></li>
+<ul>
+"""
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, "core/home.html")
+
+
+def about(request):
+    return render(request, "core/about.html")
+
+
+def portafolio(request):
+    return render(request, "core/portafolio.html")
+
+
+def contacto(request):
+    return render(request, "core/contacto.html")
--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
+    path('', views.home, name="home"),
+    path('about-me/', views.about, name="about"),
+    path('portafolio/', views.portafolio, name="portafolio"),
+    path('contacto/', views.contacto, name="contacto"),
     path('admin/', admin.site.urls),
 ]
