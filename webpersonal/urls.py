7d0216033d48eca33a7f9b2c99ab3257c10b431d
Carmor014 <carmor_014@hotmail.com>
2020-05-26 12:47:17 -0500

    Uniendo fornted con Backend

--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 
+
 urlpatterns = [
     path('', views.home, name="home"),
     path('about-me/', views.about, name="about"),
